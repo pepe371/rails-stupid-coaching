cfa7a1e23315509521198b4960cf9f0879afee23
jose garcia <joey.garcia3711@gmail.com>
2022-05-16 14:50:54 -0500

    sss

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+  def answer
+    @question = params[:prompt]
+    if @question.include?('?')
+      @answer = "Silly question, get dressed and go to work!"
+    elsif @question.include?("I am going to work")
+      @answer = "Great!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
